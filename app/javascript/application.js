cb270528b88e98484e184615549414656b124572
Markobaboci <markobaboci@gmail.com>
2025-01-27 12:30:52 +0100

    added script in application layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "bootstrap"
+import "@hotwired/turbo-rails";
+import "bootstrap";
 import "controllers";
 
-import "@popperjs/core"
+import "@popperjs/core";
 
 import "particles.js";
 
