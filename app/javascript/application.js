b9ece1b204ebf308454bb95dac3111bb66020261
Markobaboci <markobaboci@gmail.com>
2025-02-05 14:53:07 +0100

    again

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 require("@hotwired/turbo-rails");
 import "bootstrap";
 import "controllers";
-import "@popperjs/core";
+require("@popperjs/core");
 import "particles.js";
-import "controllers/typewriter_controller";
+require("controllers/typewriter_controller");
 
 document.addEventListener("turbo:load", () => {
   particlesJS.load("particles-js", "/assets/particles.json", function() {
