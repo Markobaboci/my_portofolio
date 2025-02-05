39ae336846639826f231123e5ded0c35e04e12b9
Markobaboci <markobaboci@gmail.com>
2025-02-05 14:50:20 +0100

    imported manually the typewriter

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "bootstrap";
 import "controllers";
 import "@popperjs/core";
 import "particles.js";
+import "controllers/typewriter_controller";
 
 document.addEventListener("turbo:load", () => {
   particlesJS.load("particles-js", "/assets/particles.json", function() {
